6cefa5dc242e0c9be33d65540fe002fb52391f6a
MhdJavithF <javithmd001@gmail.com>
2024-11-04 00:46:57 +0530

    calc - rating

--- calc.js
+++ calc.js
@@ -3,50 +3,43 @@ const bRef = document.getElementById('b');
 const opRef = document.getElementById('op');
 const ansRef = document.getElementById('ans');
 
-function add(aRef, bRef){
-    const a = aRef.value;
-    const b = bRef.value;
-    ansRef.innerText = a * b;
+function calculate(){
+    const a = Number(aRef.value);
+    const b = Number(bRef.value);
+
+    if(opRef.value == '+'){
+        add(a, b);
+    }
+
+    else if(opRef.value == '*'){
+        multiply(a, b);
+    }
+
+    else if(opRef.value == '-'){
+        sub(a, b);
+    }
+
+    else if(opRef.value == '/'){
+        div(a, b);
+    }
+
+    function add(a, b){
+        ansRef.innerText = a + b;
+    }
+
+    function sub(a, b){
+        ansRef.innerText = a - b;
+    }
+
+    function div(a, b){
+        ansRef.innerText = a / b;
+    }
+
+    function multiply(a, b){
+        ansRef.innerText = a * b;
+    }
 }
 
-function sub(aRef, bRef){
-    const a = aRef.value;
-    const b = bRef.value;
-    ansRef.innerText = a * b;
-}
-
-function div(aRef, bRef){
-    const a = aRef.value;
-    const b = bRef.value;
-    ansRef.innerText = a * b;
-}
-
-function multiply(aRef, bRef){
-    const a = aRef.value;
-    const b = bRef.value;
-    ansRef.innerText = a * b;
-}
-
-if(String(opRef.value) === '*'){
-    aRef.addEventListener('change',function(){
-        multiply(aRef, bRef);
-    });
-}
-
-else if(opRef.value === '+'){
-    bRef.addEventListener('change',function(){
-        add(aRef, bRef);
-    });
-}
-
-else if(opRef.value === '-'){
-    bRef.addEventListener('change',function(){
-        sub(aRef, bRef);
-    });
-}
-
-else if(opRef.value === '/'){
-    bRef.addEventListener('change',function(){
-        div(aRef, bRef);
-    });
-}
+aRef.addEventListener('input',calculate);
+bRef.addEventListener('input',calculate);
+opRef.addEventListener('change',calculate);
---
+++ rating_calc.js
@@ -0,0 +1,40 @@
+const starsRef = document.querySelector('.stars');
+const rateRef = document.querySelector('h3 span');
+const iconRef = document.querySelectorAll('.star i');
+
+let rate = 0;
+
+starsRef.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa-star')) {
+        const val = e.target.dataset.id;
+        changeColor(val);
+        rateRef.innerHTML = val
+        rate = val;
+    }
+});
+
+function changeColor(val) {
+    iconRef.forEach((icon , indx) => {
+        const curIndx = indx + 1;
+        if(curIndx <= val) {
+            icon.classList.remove('fa-regular');
+            icon.classList.add('fa-solid');
+        }
+        else {
+            icon.classList.add('fa-regular');
+            icon.classList.remove('fa-solid');
+        }
+    });
+}
+
+starsRef.addEventListener('mouseover', (e) => {
+    if(e.target.classList.contains('fa-star')) {
+        const val = e.target.dataset.id;
+        changeColor(val);
+        console.log('enter');
+    }
+});
+
+starsRef.addEventListener('mouseleave', (e) => {
+    changeColor(rate);
+});
